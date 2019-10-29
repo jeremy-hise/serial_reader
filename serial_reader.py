843f4fe97c4a16422b681d8662ce7a9bfbe39294
Jeremy Hise <jeremyhise@Jeremys-Air.home>
2019-10-29 11:12:16 -0400

    Added copyright

--- serial_reader.py
+++ serial_reader.py
@@ -1,8 +1,19 @@
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import serial
 
-
-# initiate the parser
 parser = argparse.ArgumentParser()
 parser.add_argument("-V", "--version", help="show program version", action="store_true")
 parser.add_argument("-p", "--port", required=True, help="Specify which port to read from. Example /dev/ttyUSB0")
@@ -10,13 +21,11 @@ parser.add_argument("-n", "--number",  default=0, type=int, help="Specify the nu
 parser.add_argument("-b", "--baud", default="9600", help="Specify the baud rate. Default is 9600")
 parser.add_argument("-f", "--file", help="Specify a filename to output to")
 
-# read arguments from the command line
 args = parser.parse_args()
 
 running = True
 current_it = 0
 
-# check for --version or -V
 if args.version:
     print("Serial Reader v 0.1")
 
